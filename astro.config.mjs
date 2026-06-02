d3ca48dcb09cd99e94a8a66da32613fbfe6080b1
Equipo de House Gatitos <contacto@housegatitos.com>
2026-06-02 17:19:33 -0600

    fix: change trailingSlash to ignore to allow JSON endpoints in dev server

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://housegatitos.com',
-  trailingSlash: 'always',
+  trailingSlash: 'ignore',
   integrations: [sitemap()],
   build: {
     format: 'directory',  // genera /slug/index.html en lugar de /slug.html
